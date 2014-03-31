5c76fb053a6676048ad690f4cb200cf05874020f
Ewenlong <ewenlong03@163.com>
2014-03-31 11:54:43 +0800

    Initial Commit

---
+++ yyy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  yyy
+//
+//  Created by ewenlong on 14-3-31.
+//  Copyright (c) 2014年 ewenlong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ yyy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  yyy
+//
+//  Created by ewenlong on 14-3-31.
+//  Copyright (c) 2014年 ewenlong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
